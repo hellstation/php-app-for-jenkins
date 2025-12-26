de2d9ca0a22d14bf9ccbe18604a362ac2d7ef752
qusakabi <sqwrtick1@gmail.com>
2025-12-26 17:33:30 +0300

    fuck

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1>Hello from AWS Elastic Beanstalk!</h1>";
+echo "<p>This is a basic PHP application deployed for testing purposes.</p>";
+echo "<p>Current time: " . date('Y-m-d H:i:s') . "</p>";
+echo "<p>Server: " . $_SERVER['SERVER_NAME'] . "</p>";
+?>
